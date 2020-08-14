af652c8c947a3555fc16776cd70c3593a7baf79c
Will Lytle <wlytle89@gmail.com>
2020-08-14 00:46:06 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,9 +10,7 @@ def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
   inspired = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
-  p inspired
-  inspired
-  
+  return inspired
 end
 
 def literal_aoh
